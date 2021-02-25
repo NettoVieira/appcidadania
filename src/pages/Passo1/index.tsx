c3ad79366077a211242526ea66034a06c45e0dbe
NettoVieira <joao.fvneto@hotmail.com>
2021-02-24 22:23:44 -0300

    finalizando passo1

--- src/pages/Passo1/index.tsx
+++ src/pages/Passo1/index.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import {View} from 'react-native';
 
-import {Container, Title} from './styles';
+import {
+  Container,
+  Title,
+  Scroll,
+  ContainerTitle,
+  SubTitle,
+  Paragraph,
+  ButtonHelpText,
+  GoBack,
+  GoBackIcon,
+  Text,
+} from './styles';
 
 const Passo1: React.FC = () => {
   return (
     <Container>
-      <Title>Passo1</Title>
+      <Scroll>
+        <ContainerTitle>
+          <Title>Cidadania Italiana </Title>
+        </ContainerTitle>
+        <SubTitle>Entenda como funciona</SubTitle>
+        <Paragraph>
+          A nacionalidade na Itália é regida pelo princípio do jus sanguinis.
+          Deixe-me simplificar: é italiano aquele que é descendente de um
+          Italiano. Se você sabe que tem um ascendente nascido na Itália, ou já
+          ouviu alguma tia dizer que se lembra de “ouvir vovô falar que o pai
+          dele tinha vindo da Itália”, então este App foi feito pra você.
+        </Paragraph>
+
+        <GoBack>
+          <Text>Ler introdução completa</Text>
+          <GoBackIcon name="chevron-right" size={30} color="#db822b" />
+        </GoBack>
+      </Scroll>
     </Container>
   );
 };
--- src/pages/Passo1/styles.ts
+++ src/pages/Passo1/styles.ts
@@ -1,13 +1,57 @@
 import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/Feather';
 
 export const Container = styled.View`
-  align-items: center;
-  justify-content: center;
+  margin-top: 15%;
   flex: 1;
+  padding: 6%;
 `;
 
+export const ContainerTitle = styled.View`
+  width: 80%;
+  margin-bottom: 20px;
+`;
+
+export const Scroll = styled.ScrollView``;
+
 export const Title = styled.Text`
-  font-size: 38px;
+  font-size: 30px;
+  font-family: 'Poppins-Medium';
+  color: #172239;
+  font-weight: 900;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: 20px;
+  font-family: 'Poppins-SemiBold';
+  color: #172239;
+  margin-bottom: 10px;
+`;
+
+export const Paragraph = styled.Text`
+  color: #6d6d78;
+  font-size: 14px;
   font-family: 'Poppins-Regular';
-  text-align: center;
+`;
+
+export const GoBack = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 28px;
+`;
+
+export const ButtonHelpText = styled.Text`
+  font-size: 14px;
+  color: #db822b;
+  font-family: 'Poppins-Medium';
+`;
+
+export const Text = styled.Text`
+  font-size: 18px;
+  color: #f09d4c;
+  font-family: 'Poppins-Regular';
+`;
+
+export const GoBackIcon = styled(Icon)`
+  margin-left: 2%;
 `;
